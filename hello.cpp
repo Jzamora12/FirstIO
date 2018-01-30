0118ab23d9b37fa4e900abf5b603a179774aafb7
Jzamora12 <jzamora12@ecc-jaguar.csuchico.edu>
2018-01-30 11:00:36 -0800

    changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
-
+// Author: Joseluis Zamora
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Joseluis Zamora." << std::endl;
+    std::cout << "What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
